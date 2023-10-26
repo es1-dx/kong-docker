f60f30ec39e3b6b47671f972de7b4ea55b85e746
糸山靖浩 <140064769+Itoyama-yasuhiro@users.noreply.github.com>
2023-10-26 13:30:51 +0900

    サンプルコード追加
    ・Serviceの属性取得
    ・APIコール
    ・Serviceに行く前にstatus codeを返却し、終了させる

--- kong-py-plugins/py-hello.py
+++ kong-py-plugins/py-hello.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import os
 import kong_pdk.pdk.kong as kong
+import requests
+
+from typing import TypeVar, Any, Union, List, Mapping, Tuple, Optional
+
+number = TypeVar('number', int, float)
+table = TypeVar('table', List[Any], Mapping[str, Any])
+
 
 Schema = (
     {"x-hello-from-python": {"type": "string"}},
@@ -8,7 +15,7 @@ Schema = (
 )
 
 version = '0.1.0'
-priority = 1000001
+priority = 0
 
 # This is an example plugin that add a header to the response
 
@@ -17,11 +24,40 @@ class Plugin(object):
         self.config = config
 
     def access(self, kong: kong.kong):
-      #  kong.service.set_header("application/json")
+        # header追加サンプル
         kong.service.request.set_header('x-hello-from-python','test')
+
+        # API コールサンプル
+        try:
         
-        kong.log('access')
-        kong.log(kong.router.get_service())
+           url = "http://localhost:8000/any2"
+           params = {}
+           headers = {'x-search': 'Tokyo'}
+        
+           response = requests.get(url, params=params, headers=headers)
+        
+           if response.status_code == 200:
+               data = response.json()
+               kong.log(str(data))
+           else:
+               kong.log('API Error')
+        except Exception as e:
+           kong.log(e)
+
+        # Service情報取得サンプル
+        try:
+           r = kong.router.get_service()
+
+           service_name = str(r["name"])
+           path = str(r["host"])
+           
+           kong.log.err(service_name)
+           kong.log.err(path)
+        except Exception as e:
+           kong.log(e)
+
+        # サービスに行かずに戻すサンプル
+        #kong.response.exit(407, "Test")
        
 
 # add below section to allow this plugin optionally be running in a dedicated process
